6730bcd2f303be52cfbfdd8baf5d23b18f338b57
zhaoyagu <zhao.yaguo@shinewingdigital.com>
2019-07-31 23:38:45 +0800

    reptile-node-JD

--- node爬虫/demo2-JD.html/app.js
+++ node爬虫/demo2-JD.html/app.js
@@ -1,13 +1,26 @@
 // let req = require('http')
 // 首先http模块只能发送http请求  这里使用第三方封装好的   既可以发送https 又可以发送http
 let req = require('request')
+let cheerio = require('cheerio')
 req({
-  url: `https://search.jd.com/Search?keyword=macbook%20pro&enc=utf-8&suggest=3.def.0.V10--12s0,20s0,38s0&wq=mac&pvid=a63ec670912a4d39b16750d43fb77686`,
+  url: `https://youhui.pinduoduo.com/search/landing?catId=18`,
+  encoding: 'UTF8',
   // url: `https://www.baidu.com/`,
   method: 'get'
 }, (err, res, body) => {
   if (!err && res.statusCode == 200) {
-    console.log(body, 111)
+    let _$ = cheerio.load(body)
+    // console.log(_$('.card-wrapper').html())
+    _$('.card-wrapper').children().map(function () {
+      // let url = _$(this).find('.goods-detail-card-wrapper').attr('href')
+      // console.log(_$(tclshis).html())
+      let url = 'https:' + _$(this).attr('href')
+      let imgurl = 'https:' + _$(this).find('img').attr('src')
+      let name = _$(this).children().find('.double-line-desc').text()
+      let price = _$(this).children().children().children().children().text()
+      console.log(url, imgurl, name, price)
+    })
+    // console.log(a)
   } else {
     console.log(err, 222)
   }
