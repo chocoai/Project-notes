270c234cc900c4c41f74daf375ef1a20afb1938b
zhaoyagu <zhao.yaguo@shinewingdigital.com>
2019-08-01 23:57:55 +0800

    node-reptile insertMany Mongodb yes

--- node爬虫/demo2-JD.html/app.js
+++ node爬虫/demo2-JD.html/app.js
@@ -1,7 +1,16 @@
 // let req = require('http')
 // 首先http模块只能发送http请求  这里使用第三方封装好的   既可以发送https 又可以发送http
 let req = require('request')
+// 可以向jQuery一样操作DOM
 let cheerio = require('cheerio')
+let MongoClint = require('mongodb').MongoClient
+let url = `mongodb://127.0.0.1:27017/`
+// console.log(MongoClint)
+
+
+
+
+
 req({
   url: `https://youhui.pinduoduo.com/search/landing?catId=18`,
   encoding: 'UTF8',
@@ -10,6 +19,7 @@ req({
 }, (err, res, body) => {
   if (!err && res.statusCode == 200) {
     let _$ = cheerio.load(body)
+    let pushArr = []
     // console.log(_$('.card-wrapper').html())
     _$('.card-wrapper').children().map(function () {
       // let url = _$(this).find('.goods-detail-card-wrapper').attr('href')
@@ -18,7 +28,22 @@ req({
       let imgurl = 'https:' + _$(this).find('img').attr('src')
       let name = _$(this).children().find('.double-line-desc').text()
       let price = _$(this).children().children().children().children().text()
-      console.log(url, imgurl, name, price)
+      pushArr.push({
+        url,
+        imgurl,
+        name,
+        price
+      })
+      // console.log(url, imgurl, name, price)
+    })
+    MongoClint.connect(url, {useNewUrlParser: true}, function (err, client) {
+      // console.log(client)
+      let db = client.db('zyg')
+      // console.log(db)
+      let collection = db.collection('JD')
+      collection.insertMany(pushArr, function (err, result) {
+        console.log(result)
+      })
     })
     // console.log(a)
   } else {
