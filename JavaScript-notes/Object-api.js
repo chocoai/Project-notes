7f979787d5ce20ff5ce327f1ad4808ad06d3c809
GGupzhh <1248023357@qq.com>
2019-08-28 00:32:04 +0800

    JavaScript Add Array-api update Object-api

--- JavaScript-notes/Object-api.js
+++ JavaScript-notes/Object-api.js
@@ -11,3 +11,15 @@ console.log(Object.values(obj))
 
 // [ [ 'name', 'zhh' ], [ 'age', 12 ] ]  返回一个二维数组
 console.log(Object.entries(obj))
+
+let newobj = {
+  a: 1,
+  ...obj
+}
+console.log(newobj)
+// 结构赋值 后来居上
+newobj = {
+  name: 1,
+  ...obj
+}
+console.log(newobj)
