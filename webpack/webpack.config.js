eb48e10b4bb01367983d3b923911bbb031c190a1
GGupzhh <1248023357@qq.com>
2019-08-29 23:19:02 +0800

    Add webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,6 +2,12 @@ const path  = require('path')
 module.exports = {
   // 入口 来指定一个入口起点
   entry: './app.js',
+  module: {
+    rules: [
+      {test: /\.html$/, use: 'raw-loader'}
+    ]
+  },
+  // 出口文件
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'my-first-webpack.bundle.js'
