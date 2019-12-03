6d512b73e1eca78414ecbe911c5c55a5d48b8865
GGupzhh <1248023357@qq.com>
2019-12-03 16:17:03 +0800

    feat(mywebpack): plugins ->html-webpack-plugin HTML压缩 hash命名

--- myWebpack/webpack.config.js
+++ myWebpack/webpack.config.js
@@ -1,15 +1,36 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 // webpack  是  node写的    node的写法
 module.exports = {
-  mode: 'development', // 默认两种  production（生产） development（开发）
+  // mode: 'development', // 默认两种  production（生产） development（开发）
+  mode: 'production', // 默认两种  production（生产） development（开发）  ->>>>>>> 代码会被压缩
   entry: './src/index.js', // 入口
   output: { // 出口
-    filename: 'bundle.js', // 打包后的文件名
+    // 如果想让生成的文件每次名字都不一样就可以加一个hash名称  如下 对应引入的HTML也会发生改变 而且现在每次打包之后原来的打包文件不会被清除
+    // hash后面的数字规定hash显示的位数
+    filename: 'bundle.[hash:5].js', // 打包后的文件名
     path:  path.resolve(__dirname,'dist')// 路径必须是绝对路径
   },
-  devServer: { // 开发服务器的配置
+  devServer: { // 开发服务器的配置 webpack-dev-server 会生成一个服务 
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 9000
-  }
+    port: 9000, 
+    progress: true, // 进度条
+    compress: true,  // 压缩
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // 入口的模板文件
+      filename: 'index.html', // 打包之后的文件名称
+      // html是否压缩
+      minify: {
+        // 是否删除HTML的双引号
+        removeAttributeQuotes: true,
+        // 折叠空行
+        collapseWhitespace: true,
+      },
+      // 加一个hash戳  避免缓存的问题
+      hash: true
+    })
+  ]
 }
