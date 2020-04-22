47a7ccde9c52fc804d015aaf52c65d725977d991
GGupzhh <1248023357@qq.com>
2020-04-22 14:55:04 +0800

    feat: add slots

--- 获取指定格式时间-getDate.js
+++ 获取指定格式时间-getDate.js
@@ -12,3 +12,13 @@ Date.prototype.Format = function (fmt) { // author: meizz
   for (var k in o) { if (new RegExp('(' + k + ')').test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length))) }
   return fmt
 }
+// 极其菜的排序
+let list = [1, 4, 3, 5, 8, 2]
+function slots(list) {
+  for (let i = 0 ; i < list.length; i++) {
+    setTimeout(() => {
+      console.log(list[i])
+    }, list[i] * 1000);
+  }
+}
+slots(list)
