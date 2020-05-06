f69acd960b251e23266d1d8312d4ab7f9fdaeb9a
GGupzhh <1248023357@qq.com>
2020-05-06 18:58:53 +0800

    feat(add urlcode): js-plugin-api

---
+++ JavaScript-notes/plugin-api.js
@@ -0,0 +1,13 @@
+let url = '游戏业务-报销费用';
+
+let encodeurl = encodeURI(url);
+let decodeurl = decodeURI(url);
+
+let encodeurlcom = encodeURIComponent(url);
+let decodeurlcom = decodeURIComponent(url);
+
+console.log(encodeurl);
+console.log(decodeurl);
+
+console.log(encodeurlcom);
+console.log(decodeurlcom);
